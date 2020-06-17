c58f3c254abfebb487306d5665ddc38ba8882ca8
nin-jin <sairi-na-tenshi@ya.ru>
2020-06-17 17:02:54 +0300

    Update cursor.d

--- source/jin/go/cursor.d
+++ source/jin/go/cursor.d
@@ -10,29 +10,29 @@ align(Line) struct Cursor
     /// Offset in buffer.
     align(Line) private shared size_t _offset;
 
-    ///
+    /// Offset in buffer.
     size_t offset() const
     {
         return this._offset.atomicLoad!(MemoryOrder.acq);
     }
 
-    ///
+    /// Offset in buffer. Can't be changed when finalized.
     void offset(size_t next)
     {
         assert(this._finalized == 0, "Change offset of finalized cursor");
         this._offset.atomicStore!(MemoryOrder.rel)(next);
     }
 
-    /// Finalized cursor will never change offset.
+    /// Finalized cursor shall never change offset.
     align(Line) private shared ptrdiff_t _finalized;
 
-    ///
+    /// Finalized cursor shall never change offset.
     ptrdiff_t finalized() const
     {
         return this._finalized.atomicLoad!(MemoryOrder.acq);
     }
 
-    /// 
+    /// Finalize cursor to prevent offset changes.
     void finalize()
     {
         this._finalized.atomicStore!(MemoryOrder.rel)(ptrdiff_t(-1));
